7f7f7758a272f6112b67062a08b5773b7ec5e9ed
adnankursun <adnankursun@live.com>
2020-07-22 19:03:08 -0500

    added param for string

--- jenkinsDeploy.groovy
+++ jenkinsDeploy.groovy
@@ -3,9 +3,11 @@ properties([
         booleanParam(defaultValue: false, description: 'please select to apply changes', name: 'terraformApply'), 
         booleanParam(defaultValue: false, description: 'please select to destroy everything', name: 'terraformDestroy'), 
         booleanParam(defaultValue: false, description: 'please select to run the job in debug mode', name: 'debugMode'), 
-        choice(choices: ['dev', 'qa', 'stage', 'prod'], description: 'please select the environment to deploy', name: 'environment')
+        choice(choices: ['dev', 'qa', 'stage', 'prod'], description: 'please select the environment to deploy', name: 'environment'),
+        string(defaultValue: 'None', description: 'Please provide the docker image', name: 'docker_image', trim: true)
         ])
     ])
+
 println("""
 Terraform apply: ${params.terraformApply}
 Selected env : ${params.environment}
